28a51917b099c0a0161c8e3b3fa1331bd8cda7ca
Michael Chan <mikey.chan1@gmail.com>
2018-08-25 15:48:08 -0700

    add schemas

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -1,29 +1,46 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+const mongoose = require('mongoose');
+const db = mongoose.connect(
+  'mongodb://localhost:27017/test',
+  { useMongoClient: true }
+);
+const conn = mongoose.connection;
+mongoose.Promise = global.Promise;
 
-var db = mongoose.connection;
-
-db.on('error', function() {
+conn.on('error', function() {
   console.log('mongoose connection error');
 });
 
-db.once('open', function() {
+conn.once('open', function() {
   console.log('mongoose connected successfully');
 });
 
-var itemSchema = mongoose.Schema({
-  quantity: Number,
-  description: String
+var animeSchema = mongoose.Schema({
+  name: String,
+  description: String,
+  page_url: String,
+  pic_url: String,
+  score: Number,
+  release: Date,
 });
 
-var Item = mongoose.model('Item', itemSchema);
+var movieSchema = mongoose.Schema({
+  name: String,
+  description: String,
+  page_url: String,
+  pic_url: String,
+  score: Number,
+  release: Date,
+});
+
+var Anime = mongoose.model('Anime', animeSchema);
+var Movie = mongoose.model('Movie', movieSchema);
 
 var selectAll = function(callback) {
-  Item.find({}, function(err, items) {
-    if(err) {
+  Anime.find({}, function(err, animes) {
+    if (err) {
       callback(err, null);
     } else {
-      callback(null, items);
+      callback(null, animes);
     }
   });
 };
