688fe90d2eb9fcb24e140e87390bf3133647ab03
Michael Chan <mikey.chan1@gmail.com>
2018-08-26 21:37:04 -0700

    implement search bar basic functionality

--- react-client/src/components/searchBar.jsx
+++ react-client/src/components/searchBar.jsx
@@ -1,33 +1,145 @@
 import React, { Component } from 'react';
+import {
+  withStyles,
+  MuiThemeProvider,
+  createMuiTheme,
+} from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import TheatreIcon from '@material-ui/icons/Theaters';
+import { TextField } from '@material-ui/core';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
 
-export default class SearchBar extends Component {
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  margin: {
+    margin: theme.spacing.unit,
+  },
+  cssLabel: {
+    '&$cssFocused': {
+      color: purple[500],
+    },
+  },
+  cssFocused: {},
+  cssUnderline: {
+    '&:after': {
+      borderBottomColor: purple[500],
+    },
+  },
+  bootstrapRoot: {
+    padding: 0,
+    'label + &': {
+      marginTop: theme.spacing.unit * 3,
+    },
+  },
+  bootstrapInput: {
+    borderRadius: 4,
+    backgroundColor: theme.palette.common.white,
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    padding: '10px 12px',
+    width: 'calc(100% - 24px)',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:focus': {
+      borderColor: '#80bdff',
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+    },
+  },
+  bootstrapFormLabel: {
+    fontSize: 18,
+  },
+});
+
+const theme = createMuiTheme({
+  palette: {
+    primary: green,
+  },
+});
+
+class SearchBar extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      searchTerm: '',
-    };
-    this.handleInputChange = this.handleInputChange.bind(this);
-  }
-
-  handleInputChange() {
-    this.setState({
-      searchTerm: this.search.value,
-    });
+    this.state = {};
   }
 
   render() {
+    const { classes } = this.props;
     return (
-      <form>
-        <input
-          type="text"
-          placeholder="Search for..."
-          ref={input => (this.search = input)}
-          onChange={this.handleInputChange}
-          value={this.props.value}
-          onChange={this.props.onChangeValue}
-        />
-        <p>{this.state.searchTerm}</p>
-      </form>
+      <div>
+        <form className={classes.container} noValidate autoComplete="off">
+          {/* <InputLabel
+            FormLabelClasses={{
+              root: classes.cssLabel,
+              focused: classes.cssFocused,
+            }}
+            htmlFor="custom-css-input"
+          >
+            Custom CSS
+          </InputLabel> */}
+          <TextField
+            //label="You want Anime recommendations for..."
+            type="text"
+            // placeholder="Search for..."
+            value={this.props.value}
+            onChange={this.props.onChangeValue}
+            id="full-width"
+            InputLabelProps={{ shrink: true }}
+            fullWidth
+            //margin="normal"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <TheatreIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+          <FormHelperText id="name-helper-text">
+            Search favorite movies
+          </FormHelperText>
+
+          {/* <FormControl className={classes.margin}>
+            <InputLabel
+              FormLabelClasses={{
+                root: classes.cssLabel,
+                focused: classes.cssFocused,
+              }}
+              htmlFor="custom-css-input"
+            >
+              Custom CSS
+            </InputLabel>
+            <Input
+              fullWidth
+              classes={{
+                underline: classes.cssUnderline,
+              }}
+              id="custom-css-input full-width"
+              margin="dense"
+            />
+          </FormControl> */}
+        </form>
+      </div>
     );
   }
 }
+
+export default withStyles(styles)(SearchBar);
