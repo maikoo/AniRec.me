2338cd09ed7fc3dc8384cc8e630c20023ce02064
Michael Chan <mikey.chan1@gmail.com>
2018-08-27 03:18:14 -0700

    change fonts

--- react-client/src/components/searchBar.jsx
+++ react-client/src/components/searchBar.jsx
@@ -1,71 +1,38 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import {
   withStyles,
   MuiThemeProvider,
   createMuiTheme,
-} from "@material-ui/core/styles";
-import Input from "@material-ui/core/Input";
-import InputLabel from "@material-ui/core/InputLabel";
-import FormHelperText from "@material-ui/core/FormHelperText";
-import FormControl from "@material-ui/core/FormControl";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import TheatreIcon from "@material-ui/icons/Theaters";
-import { TextField } from "@material-ui/core";
-import purple from "@material-ui/core/colors/purple";
-import green from "@material-ui/core/colors/green";
+} from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import TheatreIcon from '@material-ui/icons/Theaters';
+import { TextField } from '@material-ui/core';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
 
 const styles = theme => ({
   container: {
-    display: "flex",
-    flexWrap: "wrap",
+    display: 'flex',
+    flexWrap: 'wrap',
+    color: 'white',
   },
   margin: {
     margin: theme.spacing.unit,
   },
   cssLabel: {
-    "&$cssFocused": {
+    '&$cssFocused': {
       color: purple[500],
     },
   },
   cssFocused: {},
   cssUnderline: {
-    "&:after": {
+    '&:after': {
       borderBottomColor: purple[500],
     },
-  },
-  bootstrapRoot: {
-    padding: 0,
-    "label + &": {
-      marginTop: theme.spacing.unit * 3,
-    },
-  },
-  bootstrapInput: {
-    borderRadius: 4,
-    backgroundColor: theme.palette.common.white,
-    border: "1px solid #ced4da",
-    fontSize: 16,
-    padding: "10px 12px",
-    width: "calc(100% - 24px)",
-    transition: theme.transitions.create(["border-color", "box-shadow"]),
-    fontFamily: [
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      "Roboto",
-      '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(","),
-    "&:focus": {
-      borderColor: "#80bdff",
-      boxShadow: "0 0 0 0.2rem rgba(0,123,255,.25)",
-    },
-  },
-  bootstrapFormLabel: {
-    fontSize: 18,
   },
 });
 
@@ -82,6 +49,7 @@ class SearchBar extends Component {
   }
 
   render() {
+    const whiteFont = { color: 'white' };
     const { classes } = this.props;
     return (
       <div>
