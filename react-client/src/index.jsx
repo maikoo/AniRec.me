f3cea6741d74f90928c53df9944d26f6418fbad2
Michael Chan <mikey.chan1@gmail.com>
2018-08-26 18:20:32 -0700

    add searchbar functionality

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,35 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import $ from 'jquery';
-import List from './components/List.jsx';
+import axios from 'axios';
+import List from './components/List';
+import movieAPIKey from '../../server/movieKey';
+import SearchBar from './components/searchBar';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      items: []
-    }
+    this.state = { items: [], movieTitle: 'lord%20of%20the%20rings' };
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
-    // $.ajax({
-    //   url: '/items', 
-    //   success: (data) => {
-    //     this.setState({
-    //       items: data
-    //     })
-    //   },
-    //   error: (err) => {
-    //     console.log('err', err);
-    //   }
-    // });
+    this.fetchMovies();
   }
 
-  render () {
-    return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
-    </div>)
+  fetchMovies() {
+    const { movieTitle } = this.state;
+    const apiKey = movieAPIKey.movieAPIkey;
+    console.log(apiKey);
+    const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${movieTitle}&page=1&include_adult=false&limit=3`;
+    axios
+      .get(apiUrl)
+      .then(response => {
+        this.setState({
+          items: response.data.results,
+        });
+        console.log(this.state.items);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  handleChange(e) {
+    this.setState({ movieTitle: e.target.value }, () => {
+      if (this.state.movieTitle && this.state.movieTitle.length > 1) {
+        if (this.state.movieTitle.length % 2 === 0) {
+          this.fetchMovies();
+        }
+      }
+    });
+    console.log(this.state.movieTitle);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Item List</h1>
+        <SearchBar
+          term={this.state.movieTitle}
+          onChangeValue={this.handleChange}
+        />
+
+        <List items={this.state.items.slice(0, 3)} />
+      </div>
+    );
   }
 }
 
