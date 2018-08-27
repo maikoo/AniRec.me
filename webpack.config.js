ec37f392f86a0c368c9d3ebd5fe694fbe0120427
Michael Chan <mikey.chan1@gmail.com>
2018-08-27 03:18:38 -0700

    add file loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,35 @@
-var path = require("path");
-var SRC_DIR = path.join(__dirname, "/react-client/src");
-var DIST_DIR = path.join(__dirname, "/react-client/dist");
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/react-client/src');
+var DIST_DIR = path.join(__dirname, '/react-client/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
-    filename: "bundle.js",
+    filename: 'bundle.js',
     path: DIST_DIR,
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?/,
         include: SRC_DIR,
         exclude: /node_modules/,
-        loader: "babel-loader",
-        query: {
-          presets: ["react", "es2015"],
+        loader: 'babel-loader',
+        options: {
+          presets: ['env', 'react', 'es2015'],
         },
       },
       {
         test: /\.scss$/,
-        loaders: ["style-loader", "css-loader", "sass-loader"],
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"],
+        loaders: ['file-loader'],
       },
     ],
   },
   resolve: {
-    extensions: [".jsx", ".css", ".js", ".json"],
+    extensions: ['.jsx', '.css', '.js', '.json'],
   },
 };
