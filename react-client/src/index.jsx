b4a2a412c5f5cc167b396d9fb3429ed931ee278c
maikoo <mikey.chan1@gmail.com>
2018-08-27 01:01:59 -0700

    add material ui theme

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-// import CssBaseline from '@material-ui/core/CssBaseline';
-import App from './components/App';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import App from "./components/App";
+import "./stylesheets/main.scss";
 
-// function MyApp() {
-//   return (
-//     <React.Fragment>
-//       <CssBaseline />
-//       <App />
-//     </React.Fragment>
-//   );
-// }
+class Index extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <App />
+      </React.Fragment>
+    );
+  }
+}
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Index />, document.getElementById("app"));
