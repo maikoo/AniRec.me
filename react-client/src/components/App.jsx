20c11effeabc3637b5e2aa843a20f017525467ef
Michael Chan <mikey.chan1@gmail.com>
2018-08-29 16:53:21 -0700

    styling and cleanup

--- react-client/src/components/App.jsx
+++ react-client/src/components/App.jsx
@@ -2,19 +2,16 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 import List from './List';
 import SearchBar from './searchBar';
 import MenuAppBar from './navBar';
 import movieAPIKey from '../../../server/movieKey';
 import { findGenre } from '../genres';
-import background from '../../tenor.gif';
-
-const styles = {
-  root: {
-    width: '100%',
-    maxWidth: 500,
-  },
-};
+import background from '../../original.gif';
+import AnimeList from './animeList';
+import { withStyles } from '@material-ui/core';
+import { debounce } from 'debounce';
 
 const Image = styled.img`
   display: block;
@@ -35,6 +32,7 @@ const Container = styled.div`
   height: auto;
   width: 100%;
 `;
+
 const Section = styled.div`
   margin-right: 1.8rem;
   margin-left: 1.8rem;
@@ -51,23 +49,36 @@ const TitleDiv = styled.div`
   color: white !important;
 `;
 
-export default class App extends Component {
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    // width: '100%',
+    // maxWidth: 500,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
+
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       movies: [],
-      movieTitle: 'mission impossible',
+      movieTitle: '',
       genres: [],
       anime: [],
+      hideImage: false,
     };
     this.handleChange = this.handleChange.bind(this);
   }
 
-  componentWillMount() {
-    this.fetchMovies();
-  }
+  // componentWillMount() {}
 
   componentDidMount() {
+    // this.fetchMovies();
     // this.fetchAnime();
   }
 
@@ -82,8 +93,8 @@ export default class App extends Component {
           movies: response.data.results,
           genres: findGenre(response.data.results[0].genre_ids),
         });
-        console.log(this.state);
-        this.fetchAnime(this.state.genres[0]);
+        // console.log(this.state);
+        this.fetchAnime(this.state.genres);
       })
       .catch(err => {
         console.log(err);
@@ -91,22 +102,29 @@ export default class App extends Component {
   }
 
   fetchAnime(targetGenre) {
-    console.log(targetGenre, typeof targetGenre);
-    const aniVar = { genre_in: 'Action' };
+    console.log(targetGenre);
+    // const aniVar = targetGenre;
     const aniQuery = `
       query {
-        Page(page: 1, perPage: 5) {
-          media(genre: "${targetGenre}", averageScore_greater: 80, startDate_greater: 2015, sort: TRENDING_DESC) {
+        Page(page:1, perPage:5) {
+          media(genre_in: ${JSON.stringify(
+            targetGenre
+          )}, averageScore_greater: 70, startDate_greater: 2015 ) {
             genres,
-              title {
+            title{
               romaji
               english
-              native
             },
             averageScore,
-              startDate {
+            popularity,
+            startDate {
               year
-            }
+            },
+            episodes,
+            description,
+            coverImage {
+              large
+            },
           }
         }
       }
@@ -117,13 +135,14 @@ export default class App extends Component {
       method: 'POST',
       data: {
         query: aniQuery,
+        // variables: { genres_in: targetGenre },
       },
     })
       .then(result => {
         this.setState({
           anime: result.data.data['Page'].media,
         });
-        console.log(this.state.anime);
+        // console.log(this.state.anime);
       })
       .catch(err => {
         console.log(err);
@@ -131,18 +150,32 @@ export default class App extends Component {
   }
 
   handleChange(e) {
+    // console.log(e.key, typeof e.key);
+    // if (e.key === 'Enter') {
     this.setState({ movieTitle: e.target.value }, () => {
       if (this.state.movieTitle && this.state.movieTitle.length > 1) {
-        if (this.state.movieTitle.length % 2 === 0) {
+        if (this.state.movieTitle.length > 4) {
           this.fetchMovies();
+          this.fetchAnime(this.state.genres);
+          this.setState({ hideImage: true });
         }
       }
     });
+    // }
   }
 
   render() {
-    const overlay = styled.div``;
+    // let hidden = <Image src={background} />;
     const whiteFont = { color: 'white' };
+    const whiteBackground = {
+      border: '2px solid white',
+      borderRadius: '5px',
+      backgroundColor: 'rgba(255, 255, 255, 0.9)',
+    };
+    const { classes } = this.props;
+    // if (this.state.hideImage === true) {
+    //   hidden = null;
+    // }
     return (
       <div>
         <MenuAppBar />
@@ -155,12 +188,13 @@ export default class App extends Component {
         </Section>
         <br />
         <Container>
-          <Image src={background} />
+          <Image src={background} />;
           <TitleDiv>
-            <Typography style={whiteFont} variant="display3">
+            <Typography style={whiteFont} variant="display4">
               RecAni.me
             </Typography>
-            <Section>
+
+            <Section style={whiteBackground}>
               <SearchBar
                 term={this.state.movieTitle}
                 onChangeValue={this.handleChange}
@@ -169,8 +203,25 @@ export default class App extends Component {
           </TitleDiv>
         </Container>
         <br />
-        <List movies={this.state.movies.slice(0, 6)} />
+        <div className={classes.root}>
+          <Grid container spacing={24}>
+            <Grid item xs={6}>
+              <List
+                className={classes.paper}
+                movies={this.state.movies.slice(0, 2)}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <AnimeList
+                className={classes.paper}
+                anime={this.state.anime.slice(0, 2)}
+              />
+            </Grid>
+          </Grid>
+        </div>
       </div>
     );
   }
 }
+
+export default withStyles(styles)(App);
